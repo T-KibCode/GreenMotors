e1746eacc6028816cd8f85b5daac80a467756f7b
Twaha Kibirige <tkibirige94@gmail.com>
2025-06-23 22:17:09 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -33,4 +33,28 @@ function App() {
   );
 }
 
+function Page() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    function getTodos() {
+      const { data: todos } = await supabase.from('todos').select()
+
+      if (todos.length > 1) {
+        setTodos(todos)
+      }
+    }
+
+    getTodos()
+  }, [])
+
+  return (
+    <div>
+      {todos.map((todo) => (
+        <li key={todo}>{todo}</li>
+      ))}
+    </div>
+  )
+}
+
 export default App;
